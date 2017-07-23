7081fc54ae9d6bbdb89f61ec04e27dbe22a78063
nanolua <NanoLua1@gmail.com>
2017-07-23 22:44:47 +0430

    Update report.lua

--- plugins/report.lua
+++ plugins/report.lua
@@ -14,7 +14,7 @@ local function run(msg, matches)
    return '_ایدی مستر ذخیره شد_'
    end
  end
-    if matches[1]:lower() == 'report' and msg.reply_to_message_id_ then
+    if matches[1]:lower() == 'گزارش' and msg.reply_to_message_id_ then
   local user_name = ''
   local chat = msg.chat_id_
   local master = ldata[tostring(chat)]["master"]
@@ -26,7 +26,7 @@ local function run(msg, matches)
     else
      user_name = data.first_name_
     end
-    tdcli.sendMessage(msg.chat_id_, 0, 1,  '_گزارش شما به مدیر گروه ارسال شد._', 1, 'md')
+    tdcli.sendMessage(msg.chat_id_, 0, 1,  '_گزارش* شما به مدیر گروه ارسال شد.*_', 1, 'md')
     tdcli.sendMessage(master, 0, 1, '[ <code>'..msg.sender_user_id_..'</code> ] '..user_name..' <b>send to you a report:</b>', 1, 'html')
     tdcli.forwardMessages(master, msg.chat_id_,{[0] = msg.reply_to_message_id_}, 0,dl_cb,nil)
    end
@@ -37,7 +37,7 @@ local function run(msg, matches)
     end
 end
 
-return { patterns = { '^[/!#]([Rr]eport)$', '^[/!#]([Ss]etmaster) (%d+)$' }, run = run }
+return { patterns = { '^(گزارش)$', '^[/!#]([Ss]etmaster) (%d+)$' }, run = run }
 
 -- END
 
